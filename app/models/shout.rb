0247f2a6f5f3fba30d028ce32eb99a8df3d28b43
Evan Morikawa <evan@evanmorikawa.com>
2012-03-12 14:24:38 -0400

    Shouts now polymorphic and displaying

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,5 +1,6 @@
 class Shout < ActiveRecord::Base
   belongs_to :user
+  belongs_to :medium, polymorphic: true
 
   def self.current
     order("created_at DESC") # Standard query syntax. returns active record relation.
