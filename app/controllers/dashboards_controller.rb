eddfb71324c09478f99c0643828f860d12c62cda
Evan Morikawa <evan@evanmorikawa.com>
2012-03-12 15:06:58 -0400

    PhotoShouts working now

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @text_shout = TextShout.new
+    @photo_shout = PhotoShout.new
     #
     # If you used current_user.shouts.all, the query would be executed immediately.
     @shouts = current_user.shouts.current
